18470f0868837a537c45382c04b6d5fe8752602b
Aaron Henry <aaronhenry1994@outlook.com>
2025-07-29 08:07:44 -0600

    Populated the gameboard.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,24 +1,30 @@
 class Node {
-    constructor0(x, y) {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     };
 }
 
 class Gameboard {
+
+    board = [];
     
     createBoard() {
         for (let x = 0; x < 10; x++) {
             for (let y = 0; y < 10; y++) {
                 let cell = new Node(x, y);
-            }
+                this.board.push(cell);
+            };
         };
-        
+        return this.board
+    };
 
-        
+    placeShip() {
+
     };
 };
 
-let board = new Gameboard;
+let block = new Gameboard;
+block.createBoard();
 
 module.exports = Gameboard;
--- tests/board.test.js
+++ tests/board.test.js
@@ -3,5 +3,5 @@ const Gameboard = require('../src/gameboard');
 let box = new Gameboard;
 
 test ('Creates a 10 by 10 board', () => {
-    expect(box.createBoard(10, 10)).toBe(10, 10);
+    expect(box.createBoard()).toHaveLength(100);
 })

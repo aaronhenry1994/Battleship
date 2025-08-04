e3e254a7ec26fcbb171452f71cb1abeb5c198d74
Aaron Henry <aaronhenry1994@outlook.com>
2025-08-04 08:15:56 -0600

    Made it so that I can place ships horizontally or vertically.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,5 @@
+const Ship = require('../src/ship');
+
 class Node {
     constructor(x, y) {
         this.x = x;
@@ -19,14 +21,36 @@ class Gameboard {
         return this.board
     };
 
-    placeShip(x, y) {
+    placeShip(x, y, length, horizontal, vertical) {
         let coordinates = [];
         position = {
             x: x,
             y: y,
         };
+
         coordinates.push(position);
-        return coordinates;
+
+
+
+        if (length = 1) {
+            return boat = new Ship(coordinates, 1, 0, false);
+        }
+        if (length = 2 && horizontal === true) {
+            coordinates.push((x+1, y))
+            return boat = new Ship(coordinates, 2, 0, false, horizontal);
+        };
+        if (length = 3 && horizontal === true) {
+            coordinates.push((x+2, y));
+            return boat = new Ship(coordinates, 3, 0, false, horizontal);
+        };
+        if (length = 2 && vertical === true) {
+            coordinates.push((x, y + 1))
+            return boat = new Ship(coordinates, 2, 0, false, vertical);
+        };
+        if (length = 3 && vertical === true) {
+            coordinates.push((x, y + 2));
+            return boat = new Ship(coordinates, 3, 0, false, vertical);
+        };
     };
 };
 
--- tests/board.test.js
+++ tests/board.test.js
@@ -6,6 +6,6 @@ test ('Creates a 10 by 10 board', () => {
     expect(box.createBoard()).toHaveLength(100);
 })
 
-test ('Returns a cordinate', () => {
-    expect(box.placeShip(1, 2)).toEqual([{x: 1, y: 2}]);
-})
+test ('Returns a ship with a length of a two horizontally', () => {
+    expect(box.placeShip(1, 2, 2, true, false)).toEqual({"holes": 0, "long": 1, "position": [{"x": 1, "y": 2}], "sunk": false} );
+});

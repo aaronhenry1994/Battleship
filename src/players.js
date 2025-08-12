21bc6136f2713fa8e1e040c49ac77d59b1169c26
Aaron Henry <aaronhenry1994@outlook.com>
2025-08-12 08:15:33 -0600

    Began working on html

--- src/players.js
+++ src/players.js
@@ -9,19 +9,15 @@ class Player {
 
 class Computer {
 
-    computerTurn(x, y) {
+    computerTurn() {
         
-        numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-        const randomNum = numbers[Math.floor(Math.random() * numbers.length)];
+        const randomX = numbers[Math.floor(Math.random() * numbers.length)];
+        const randomY = numbers[Math.floor(Math.random() * numbers.length)];
 
-        coordinates = [x, y]
-
-        x = randomNum;
-        y = randomNum;
-
-        return coordinates;
-    }
+        return [randomX, randomY];
+    };
 };
 
 module.exports = Player;
---
+++ tests/players.test.js
@@ -0,0 +1,4 @@
+const Player = require('../src/players');
+const Computer = require('../src/players');
+
+let cpu = new Computer;

572427a561a736666e9abe661fa3d070a8d06f0c
Aaron Henry <aaronhenry1994@outlook.com>
2025-08-13 17:47:04 -0600

    Added another board for the cpu to use.

--- src/ui.js
+++ src/ui.js
@@ -3,16 +3,31 @@ import { Ship } from '../src/ship.js';
 import { Player } from '../src/players.js';
 import { Computer } from '../src/players.js';
 
-const grid = document.getElementById('grid-container');
+const playerBoard = document.getElementById('player-board');
+const cpuBoard = document.getElementById('cpu-board');
 
 for (let x = 0; x < 10; x++) {
             for (let y = 0; y < 10; y++) {
                 const boxDiv = document.createElement('div');
+                boxDiv.id = x + '-' +y;
                 boxDiv.style.width = '50px';
                 boxDiv.style.height = '50px';
                 boxDiv.style.backgroundColor = 'lightblue';
                 boxDiv.style.border = '2px solid navy';
                 boxDiv.style.margin = '1px';
-                grid.appendChild(boxDiv);
+                playerBoard.appendChild(boxDiv);
+            };
+        };
+
+for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {
+                const boxDiv = document.createElement('div');
+                boxDiv.id = x + '-' +y;
+                boxDiv.style.width = '50px';
+                boxDiv.style.height = '50px';
+                boxDiv.style.backgroundColor = 'lightblue';
+                boxDiv.style.border = '2px solid navy';
+                boxDiv.style.margin = '1px';
+                cpuBoard.appendChild(boxDiv);
             };
         };

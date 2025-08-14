4e4f68acc5976b1addfc3690ff2941899825475d
Aaron Henry <aaronhenry1994@outlook.com>
2025-08-14 08:11:54 -0600

    Added event listeners to the boards.

--- src/ui.js
+++ src/ui.js
@@ -6,16 +6,22 @@ import { Computer } from '../src/players.js';
 const playerBoard = document.getElementById('player-board');
 const cpuBoard = document.getElementById('cpu-board');
 
+let humanBoard = new Gameboard();
+let computerBoard = new Gameboard();
+
 for (let x = 0; x < 10; x++) {
             for (let y = 0; y < 10; y++) {
                 const boxDiv = document.createElement('div');
                 boxDiv.id = x + '-' +y;
-                boxDiv.style.width = '50px';
-                boxDiv.style.height = '50px';
+                boxDiv.style.width = '40px';
+                boxDiv.style.height = '40px';
                 boxDiv.style.backgroundColor = 'lightblue';
                 boxDiv.style.border = '2px solid navy';
                 boxDiv.style.margin = '1px';
                 playerBoard.appendChild(boxDiv);
+                boxDiv.addEventListener('click', () => {
+                    console.log(boxDiv.id);
+                })
             };
         };
 
@@ -23,11 +29,14 @@ for (let x = 0; x < 10; x++) {
             for (let y = 0; y < 10; y++) {
                 const boxDiv = document.createElement('div');
                 boxDiv.id = x + '-' +y;
-                boxDiv.style.width = '50px';
-                boxDiv.style.height = '50px';
+                boxDiv.style.width = '40px';
+                boxDiv.style.height = '40px';
                 boxDiv.style.backgroundColor = 'lightblue';
                 boxDiv.style.border = '2px solid navy';
                 boxDiv.style.margin = '1px';
                 cpuBoard.appendChild(boxDiv);
+                boxDiv.addEventListener('click', () => {
+                    console.log(boxDiv.id);
+                })
             };
         };
